86aafe87ad9b42f398df0b4fdf66b791d0cc2aa6
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-11 21:31:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname = document.getElementById('fname').value;
+	let lname = document.getElementById('lname').value;
+	let statement = `${fname} ${lname}`;
+	
+	alert(statement); 
 }
